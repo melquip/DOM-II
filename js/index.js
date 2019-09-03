a1bea03fedcaab9d7ed5bf0e74eef3d7653624ef
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-03 12:47:13 +0100

    Added mouseover, mouseout, scroll event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+let element = window;
+document.querySelectorAll('h2').forEach((title) => title.addEventListener('mouseover', function(e) {
+	title.style.color = 'red';
+}));
+document.querySelectorAll('h2').forEach((title) => title.addEventListener('mouseout', function(e) {
+	title.style.color = 'black';
+}));
+element.addEventListener('wheel', function(e) {
+
+});
+element.addEventListener('drag', function(e) {
+
+});
+element.addEventListener('load', function(e) {
+
+});
+window.addEventListener('resize', function(e) {
+	alert("Dont resize!");
+});
+document.querySelector('body').addEventListener('scroll', function(e) {
+	console.log('Scrolling now...');
+});
+element.addEventListener('select', function(e) {
+
+});
+element.addEventListener('dblclick', function(e) {
+
+});
