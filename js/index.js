8e11adf165bbb3aa93e74fa97f02416fd091ca8e
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-03 13:20:45 +0100

    Added click event and changed to mouseenter and mouseleave

--- js/index.js
+++ js/index.js
@@ -1,37 +1,47 @@
 // Your code goes here
 let element = window;
-document.querySelectorAll('h2').forEach((title) => title.addEventListener('mouseover', function(e) {
-	title.style.color = 'red';
-}));
-document.querySelectorAll('h2').forEach((title) => title.addEventListener('mouseout', function(e) {
-	title.style.color = 'black';
-}));
-element.addEventListener('wheel', function(e) {
 
-});
-element.addEventListener('drag', function(e) {
+let body = document.querySelector('body');
+let allHeading2 = document.querySelectorAll('h2');
+let allParagraphs = document.querySelectorAll('p');
+let images = document.querySelectorAll('img');
 
-});
-element.addEventListener('load', function(e) {
-
-});
-window.addEventListener('resize', function(e) {
-	alert("Dont resize!");
-});
-document.querySelector('body').addEventListener('scroll', function(e) {
-	console.log('Scrolling now...');
-});
-element.addEventListener('select', function(e) {
-
-});
-document.querySelectorAll('p').forEach((paragraph) => paragraph.addEventListener('dblclick', function(e) {
-	if (!navigator.clipboard) {
-		alert('You can\'t doubleclick this content');
-		return;
-	}
-	navigator.clipboard.writeText(this.textContent).then(function() {
-		alert('You copied the paragraph!');
-	}, function(err) {
-		console.error('Could not copy text: ', err);
+window.addEventListener('load', function(e) {
+	console.log('Hello! Welcome to Fun Bus website!');
+	// mouseenter
+	allHeading2.forEach(title => title.addEventListener('mouseenter', function(e) {
+		title.style.color = 'red';
+	}));
+	// mouseleave
+	allHeading2.forEach(title => title.addEventListener('mouseleave', function(e) {
+		title.style.color = 'black';
+	}));
+	// click
+	images.forEach(image => image.addEventListener('click', function(e) {
+		this.style.display = 'none';
+	}));
+	
+	element.addEventListener('drag', function(e) {
+	
 	});
-}));
+
+	window.addEventListener('resize', function(e) {
+		console.log("Dont resize!");
+	});
+	
+	body.addEventListener('scroll', function(e) {
+		console.log('Scrolling now...');
+	});
+	
+	allParagraphs.forEach(paragraph => paragraph.addEventListener('dblclick', function(e) {
+		if (!navigator.clipboard) {
+			alert('You can\'t doubleclick this content');
+			return;
+		}
+		navigator.clipboard.writeText(this.textContent).then(function() {
+			alert('You copied the paragraph!');
+		}, function(err) {
+			console.error('Could not copy text: ', err);
+		});
+	}));
+});
