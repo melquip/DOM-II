be99b6ab418e2c6aa31af8597c802561c61a211c
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-03 13:43:14 +0100

    Added mousedown, mouseup, mousemove and mouseleave events to buttons

--- js/index.js
+++ js/index.js
@@ -5,34 +5,67 @@ let body = document.querySelector('body');
 let allHeading2 = document.querySelectorAll('h2');
 let allParagraphs = document.querySelectorAll('p');
 let images = document.querySelectorAll('img');
-
+let buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => button.setAttribute('data-text', button.textContent));
+// load event
 window.addEventListener('load', function(e) {
 	console.log('Hello! Welcome to Fun Bus website!');
-	// mouseenter
+	body.style.opacity = 1;
+	// mouseenter event
 	allHeading2.forEach(title => title.addEventListener('mouseenter', function(e) {
 		title.style.color = 'red';
 	}));
-	// mouseleave
+	// mouseleave event
 	allHeading2.forEach(title => title.addEventListener('mouseleave', function(e) {
 		title.style.color = 'black';
 	}));
-	// click
+	// click event
 	images.forEach(image => image.addEventListener('click', function(e) {
 		this.style.display = 'none';
 	}));
 	
-	element.addEventListener('drag', function(e) {
-	
-	});
-
+	let isDragging = false;
+	function btnMouseMove(e) {
+		console.log('Dragging a button');
+		isDragging = true;
+		this.classList.add('isDragging');
+		this.textContent = "Stop dragging me!";
+	}
+	// mousedown event
+	buttons.forEach(button => button.addEventListener('mousedown', function(e) {
+		if(e.which === 1 && !isDragging) {
+			// mousemove event
+			this.addEventListener('mousemove', btnMouseMove);
+		}
+	}));
+	// mouseup event
+	buttons.forEach(button => button.addEventListener('mouseup', function(e) {
+		console.log('Dropped a button');
+		isDragging = false;
+		this.classList.remove('isDragging');
+		this.textContent = this.getAttribute('data-text');
+		this.removeEventListener('mousemove', btnMouseMove);
+	}));
+	// mouseleave event triggering mouseup event
+	buttons.forEach(button => button.addEventListener('mouseleave', function(e) {
+		let event = document.createEvent("HTMLEvents");
+		event.initEvent("mouseup", true, true);
+		event.eventName = "mouseup";
+		button.dispatchEvent(event);
+	}));
+	// resize event
 	window.addEventListener('resize', function(e) {
 		console.log("Dont resize!");
+		body.style.opacity *= 0.99; 
 	});
 	
+	// scroll event
 	body.addEventListener('scroll', function(e) {
 		console.log('Scrolling now...');
+		
 	});
 	
+	// double click event
 	allParagraphs.forEach(paragraph => paragraph.addEventListener('dblclick', function(e) {
 		if (!navigator.clipboard) {
 			alert('You can\'t doubleclick this content');
