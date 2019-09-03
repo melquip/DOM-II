1f9981bafb328f388a0613d73054888260219050
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-03 15:20:49 +0100

    Stretch: Added animation to rockets transported to the top

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -21,26 +21,45 @@
 let container = document.querySelector('.blocks');
 let blocks = document.querySelectorAll('.block');
 
-let blockMouseDown = false;
-blocks.forEach(block => {
+let isMouseDown = false;
+let blockMouseDown;
+let blockCanAnim = true;
+let blockGoLeft;
+blocks.forEach((block, i) => {
+	block.style.transform = 'translateY(0)';
+	block.style.transition = 'transform 1s ease-in-out';
+	block.style.marginLeft = '0px';
 	block.addEventListener('mouseup', function(e) {
-		if(e.which === 1) {
-			container.prepend(block);
+		if(e.which === 1 && blockCanAnim) {
+			blockCanAnim = false;
+			clearTimeout(blockGoLeft);
+			let index = Array.from(container.children).indexOf(this);
+			this.style.transform = 'translateY(calc(-'+(index*100)+'% - '+(index*20)+'px))';
+			setTimeout(() => {
+				console.log(this);
+				blockCanAnim = true;
+				blocks.forEach((b, i) => b.style.transition = 'transform 1s ease-in-out');
+				this.style.transition = 'transform 0s ease-in-out';
+				this.style.transform = 'translateY(0)';
+				container.prepend(block);
+			}, 1000);
 		}
 	});
-	block.style.marginLeft = '0%';
 	block.addEventListener('mousedown', function() {
-		blockMouseDown = block;
+		blockGoLeft = setTimeout(function() {
+			blockMouseDown = block;
+			isMouseDown = true;
+		}, 100);
 	});
 });
 
 container.addEventListener('mouseup', function(e) {
-	blockMouseDown = false;
+	isMouseDown = false;
 });
 
 setInterval(function() {
-	if(blockMouseDown) {
-		blockMouseDown.style.marginLeft = Number(blockMouseDown.style.marginLeft) + 1 + '%';
+	if(isMouseDown) {
+		blockMouseDown.style.marginLeft = parseInt(blockMouseDown.style.marginLeft) + 1 + 'px';
 	}
 }, 10);
 
